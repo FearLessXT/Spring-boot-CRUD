04b2ce0b7d588e449ee250e4abc6cb9fc0b63987
munyraksmey99@gmail.com <munyraksmey.m@pipay.com>
2023-08-09 15:02:29 +0700

    Improvement: Implement File upload/download

---
+++ src/main/java/com/example/demo/product/FileUploadController.java
@@ -0,0 +1,23 @@
+package com.example.demo.product;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/v1/file")
+public class FileUploadController {
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+
+        File convertFile = new File("/var/tmp/" + file.getOriginalFilename());
+        convertFile.createNewFile();
+        FileOutputStream fout = new FileOutputStream(convertFile);
+        fout.write(file.getBytes());
+        fout.close();
+        return "File is upload successfully";
+    }
+}
